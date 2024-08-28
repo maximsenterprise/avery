570fa5eff44d39d0b7df6474734eb6999eafe21b
Maxims Enterprise <maximsenterprise@gmail.com>
2024-08-28 16:49:43 +0200

    Removed old Fat16 driver

--- avery/init.c
+++ avery/init.c
@@ -8,7 +8,6 @@
 */
 
 #include "fision/terminal.h"
-#include "fs.h"
 #include "input.h"
 #include "interrupts/gdt.h"
 #include "interrupts/idt.h"
@@ -33,11 +32,6 @@ void init() {
     out("Avery Kernel\n");
     out("Version Alpha 0.0.1\n");
     out("Created by Maxims Enterprise\n\n");
-    hdd_setup();
-
-    char file_list[1024];
-    unsigned long file_list_size = sizeof(file_list);
-    os_fat16_get_file_list(file_list, file_list_size);
 
     while (1) {
         char* in = input("> ");
--- include/fs.h
+++
@@ -1,7 +0,0 @@
-#ifndef FS_H
-#define FS_H
-
-extern void os_fat16_get_file_list(char *buffer, unsigned long buffer_size);
-extern void hdd_setup();
-
-#endif  // FS_H
